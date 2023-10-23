676b75b01a09215ed63583716fadc83911e93656
lahi1998 <sadolinlars@gmail.com>
2023-10-23 08:36:05 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CvComponent } from './Components/cv/cv.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cv', component: CvComponent },
+  { path: 'project', component: ProjectsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CvComponent } from './Components/cv/cv.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent,
+    ProjectsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
