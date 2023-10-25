a85904921a829c8f4239a76217c4dbd8fed40364
lahi1998 <sadolinlars@gmail.com>
2023-10-25 15:04:17 +0200

    added 2 projecter

--- src/app/Components/projects/projects.component.ts
+++ src/app/Components/projects/projects.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+  constructor() { }
 
+  slides: string [] = ['./assets/projects/UdlaansideZbc/Login.jpg', './assets/projects/UdlaansideZbc/Forside.jpg', 
+  './assets/projects/UdlaansideZbc/Udlaan.jpg', './assets/projects/UdlaansideZbc/SletPordukt.jpg','./assets/projects/UdlaansideZbc/OpretPordukt.jpg',
+  './assets/projects/UdlaansideZbc/AdminTables.jpg', './assets/projects/UdlaansideZbc/AdminBrugerUpdate.jpg', './assets/projects/UdlaansideZbc/AdminBrugere.jpg', ]
+  i=0;
+
+  getSlide() {
+      return this.slides[this.i];
+  }
+
+  getPrev() {
+      this.i = this.i===0 ? 0 : this.i - 1;
+  }
+//edit here    
+  getNext() {
+    
+    if(this.i == 7)
+    { 
+      this.i = 0;
+      console.log(this.i)
+    }
+    else{
+      this.i = this.i===this.slides.length ? this.i : this.i + 1;
+      console.log(this.i)
+    }
+    
+  }
 }
